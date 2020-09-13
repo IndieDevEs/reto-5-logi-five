0491216200e8a4e3a4c380d187335aeb0fdcbdd3
Juan José Montes de Oca Arbós <juanjose.montesdeocaarbos@gmail.com>
2020-09-13 00:51:37 -0300

    Se encapsula lectura/escritura de valor de Bloque en métodos

--- jjmontes/Block/Block.gd
+++ jjmontes/Block/Block.gd
@@ -6,22 +6,25 @@ var row
 var column
 var group
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = values[0]
+	set_value(" ")
 
 func _on_Block_gui_input(event):
 	if event is InputEventMouseButton and event.pressed == true and event.button_index == BUTTON_LEFT:
-		var pos = values.find($Label.text)
+		var pos = values.find(get_value())
 		if (pos == -1):
 			pos = 0
 		pos += 1
 		if (pos == values.size()):
 			pos = 0
 		
-		$Label.text = values[pos]
+		set_value(values[pos])
+
+func set_value(new_value):
+	if values.find(new_value) > -1:
+		$Label.text = new_value
+	else:
+		$Label.text = values[0]
+
+func get_value():
+	return $Label.text
